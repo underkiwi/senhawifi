b1d44a497d93b16dc99c0f101bc95bddb4ebfe44
underkiwi <75700620+underkiwi@users.noreply.github.com>
2020-12-08 18:32:03 -0300

    Update and rename network_details.js.example to network_details.js

---
+++ network_details.js
@@ -0,0 +1,7 @@
+var network_details = {
+    address: 'that place where I put that thing that time',
+    network_name: 'gremio',
+    password_as_spoken: 'frida dois mil e dezesseis'
+}
+
+module.exports = network_details;
